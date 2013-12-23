3ed9d708a632b75de6738cd2d7d4387aa4dfa404
Aimee Knight <AimeeKnight83@gmail.com>
2013-12-23 11:43:19 -0500

    ad view functions

--- blog/admin.py
+++ blog/admin.py
@@ -9,7 +9,7 @@ class PostAdmin(admin.ModelAdmin):
 	#fields to search in change list
 	search_fields = ['title', 'description', 'content']
 	#enable the date drill down on change list
-	#date_hierarchy = 'created'
+	date_hierarchy = 'created'
 	#enable the save buttons on top on change form
 	save_on_top = True
 	#prepopulate the slug from the title

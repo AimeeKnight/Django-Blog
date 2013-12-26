ed96431e8ce75cb0e1cdd936ff7c06c8e5861a19
Aimee Knight <AimeeKnight83@gmail.com>
2013-12-26 11:09:58 -0500

    install bootstrap and work on UI/UX

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'bootstrap_toolkit',
 )
 
 MIDDLEWARE_CLASSES = (
